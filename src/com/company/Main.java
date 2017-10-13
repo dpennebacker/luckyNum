5cd5d115a0cd2195463a19ca6175d26f4a29bd68
Evan Older <evan.older@gmail.com>
2017-10-13 08:39:50 -0400

    never commited, my bad

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sysin = new Scanner(System.in);
+        System.out.println("What is the name of the person you want a lucky number for?");
+        String name = sysin.nextLine();
+        person unknown = new person(name);
+        unknown.welcomeStats();
+        while(true) {
+            System.out.println("What would you like to do? Change name, change number, or are you done?");
+            String res = sysin.nextLine();
+            if (res.toLowerCase().contains("name")) {
+                System.out.println("What name would you like to change to?");
+                unknown.newName(sysin.nextLine());
+                unknown.welcomeStats();
+            }
+            if (res.toLowerCase().contains("number")) {
+                unknown.newNum();
+                unknown.printStats();
+            }
+            if(res.toLowerCase().contains("done")) {
+                System.out.println("Have a nice day!");
+                System.exit(1);
+            }
+        }
     }
 }
